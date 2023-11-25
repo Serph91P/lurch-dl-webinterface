10927ee993abaf84cb8d04a3ba48f3d1054d8a1b
Julian Müller (ChaoticByte) <contact@chaoticbyte.net>
2023-11-25 10:21:41 +0100

    Changed the build-container script to use docker by default and podman if specified

--- build-container.sh
+++ build-container.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
+# Usage:
+## Docker
+# sudo ./build-container.sh
+## Podman
+# ./build-container.sh podman
+
 VERSION=$(git describe --tags)
 
 echo "Building version ${VERSION} as container..."
 
-podman build -t "lurch-dl:${VERSION}" --build-arg="VERSION=${VERSION}" .
+if [[ "$1" == "podman" ]]; then
+    podman build -t "lurch-dl:${VERSION}" --build-arg="VERSION=${VERSION}" .
+else
+    docker build -t "lurch-dl:${VERSION}" --build-arg="VERSION=${VERSION}" .
+fi
